316a0c40465780f68e13707b8b73421bda86d9cb
JHOSUA ESTEBAN <2232516@slu.edu.ph>
2024-03-28 00:43:19 +0800

    created an InvalidInputException for input validation

---
+++ src/main/java/midproject/SharedClasses/UserDefinedExceptions/InvalidInputException.java
@@ -0,0 +1,7 @@
+package midproject.SharedClasses.UserDefinedExceptions;
+
+public class InvalidInputException extends Exception {
+    public InvalidInputException(String message) {
+        super(message);
+    }
+}
